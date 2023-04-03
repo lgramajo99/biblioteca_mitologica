78b3f78190d8b0850bacdfb4cd99531195bce8f0
lgramajo99 <lucianongramajo@gmail.com>
2023-04-03 01:35:10 -0300

    Se agregan unos cambios a Footer.jsx;

--- front/src/components/footer/Footer.jsx
+++ front/src/components/footer/Footer.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
+import style from "./Footer.module.css";
 
-export default function Footer() {
-    return (<div>
-        <ul>
-            <li><p>&copy;<code>Biblioteca Mitologica</code> Creado en Enero de 2023</p></li>
-            <li><Link to="/">Acerca de Biblioteca Mitologica</Link></li>
-            <li><Link to="/">Política de Privacidad</Link></li>
-            <li><Link to="/">Categorias</Link></li>
-            <li><Link to="/">Contacto</Link></li>
-            <li><Link to="/">¿Encontró un error?</Link></li>
-        </ul>
-    </div>)
+function Footer() {
+    return (
+        <div className={style.footer}>
+            <div className={style.contenedor}>
+                <p>&copy; 2023 <code>Biblioteca Mitologica.</code></p>
+            <hr  className={style.hr}/>
+                <ul className={style.lista}>
+                    <li><Link className={`${style.links}`} to="/">Política de Privacidad</Link></li>
+                    <li><Link className={`${style.links}`} to="/">Categorias</Link></li>
+                    <li><Link className={`${style.links}`} to="/">¿Encontró un error?</Link></li>
+                    <li><Link className={`${style.links}`} to="/">Contacto</Link></li>
+                    <li><Link className={`${style.links}`} to="/">Acerca de Biblioteca Mitologica</Link></li>
+                </ul>
+            </div>
+        </div>)
 }
+
+export default Footer;
