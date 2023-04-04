22f224c46107d9d4454ce78331aca1fba561386d
lgramajo99 <lucianongramajo@gmail.com>
2023-04-03 21:01:23 -0300

    agregando estilos a Card.jsx;

--- front/src/components/pages/card/Card.jsx
+++ front/src/components/pages/card/Card.jsx
@@ -1,15 +1,16 @@
 import styles from './Card.module.css'
+import { Link } from 'react-router-dom';
 
 function Card() {
     let pruebaImg = 'https://www.zooplus.es/magazine/wp-content/uploads/2022/05/Cuanto-pesa-un-gato-2.jpeg';
 
     return (<div className={styles.contenedor}>
         <img className={styles.imagen} src={pruebaImg} alt="Imagen de algo" />
-        <h3 className={styles.nombre}>Nombre Egiptico</h3>
-        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-        <button className={styles.botonEntrar} disabled="disabled">Entrar</button>
+        <footer className={styles.footer}>
+            <Link to={'/#'} className={styles.nombre}>Nombre Egipcio</Link>
+            <p className={styles.copete}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </footer>
     </div>)
 }
-
 
 export default Card;
