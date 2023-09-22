b20e4e2fcd16f59d2ddf6937726fed3e13a13838
lgramajo99 <lucianongramajo@gmail.com>
2023-09-21 22:43:19 -0300

    Empezamos a manejar la estructura de las secciones, +vistas y +recientes

---
+++ client/src/components/Manufactures.jsx
@@ -0,0 +1,17 @@
+import Card from "./Card";
+
+function Manufactures() {
+    return (
+        <div>
+            <h1>Seccion articulos: +Vistos; +Recientes</h1>
+            <Card />
+            <Card />
+            <Card />
+
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+export default Manufactures;
--- client/src/pages/Manufactures.jsx
+++
@@ -1,6 +0,0 @@
-function Manufactures() {
-    return (
-        <div><h1>Pagina de articulos</h1></div>
-    )
-}
-export default Manufactures;
