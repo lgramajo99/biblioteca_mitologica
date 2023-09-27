a9df945f391e28dce948968f2499a5b28e4a077f
lgramajo99 <lucianongramajo@gmail.com>
2023-09-26 21:50:20 -0300

    se agrega e instala framework tailwind

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Biblioteca mitologica</h1>
+            <h1 className="bg-slate-800">Biblioteca mitologica</h1>
         </div>
     )
 }
