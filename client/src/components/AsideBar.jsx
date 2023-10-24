abb5eddc31da813e013cf38b036aa4e3afd0cc0d
lgramajo99 <lucianongramajo@gmail.com>
2023-10-24 18:19:58 -0300

    Componente Asidebar creado

---
+++ client/src/components/AsideBar.jsx
@@ -0,0 +1,19 @@
+function AsideBar() {
+    return (<aside>
+        <h1>Lista de mitologias</h1>
+        <ul>
+            <li>Mitol - #1</li>
+            <li>Mitol - #2</li>
+            <li>Mitol - #3</li>
+            <li>Mitol - #4</li>
+            <li>Mitol - #5</li>
+            <li>Mitol - #6</li>
+            <li>Mitol - #7</li>
+            <li>Mitol - #8</li>
+            <li>Mitol - #9</li>
+            <li>Mitol - #0</li>
+        </ul>
+    </aside>)
+}
+
+export default AsideBar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Card from "../components/Card";
+import AsideBar from "../components/AsideBar";
 import Manufactures from "../components/Manufactures";
 
 function Home() {
@@ -12,22 +12,11 @@ function Home() {
                     <Manufactures seccion={'Mas vistos'} />
                 </article>
                 <aside className="md:w-3/12 lg:w-2/12">
-                    Lista de mitologias
-                    <ul>
-                        <li>Mitol - #1</li>
-                        <li>Mitol - #2</li>
-                        <li>Mitol - #3</li>
-                        <li>Mitol - #4</li>
-                        <li>Mitol - #5</li>
-                        <li>Mitol - #6</li>
-                        <li>Mitol - #7</li>
-                        <li>Mitol - #8</li>
-                        <li>Mitol - #9</li>
-                        <li>Mitol - #0</li>
-                    </ul>
+                    <AsideBar />
                 </aside>
             </secction >
         </section>
     )
 }
+
 export default Home;
