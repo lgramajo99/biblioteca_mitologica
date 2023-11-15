ccbd1dc9daf2e6414b0d43d0d72dc445c67f6422
lgramajo99 <lucianongramajo@gmail.com>
2023-11-15 17:12:52 -0300

    Estructura de footer lista. falta darle diseños y mas detalles.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   )
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,34 @@
 function Footer() {
-    return (<div>
-        <h1>Navbar</h1>
-    </div>)
+    return (
+        <footer>
+            <section>
+                <p>Seccion de redes sociales.</p>
+
+                <a href="*" target="_blank" rel="noopener noreferrer">💧</a>
+                <a href="*" target="_blank" rel="noopener noreferrer">🔥</a>
+                <a href="*" target="_blank" rel="noopener noreferrer">❄</a>
+                <a href="*" target="_blank" rel="noopener noreferrer">⚡</a>
+
+            </section>
+
+            <section>
+                <p>Seccion de sobre el blog.</p>
+
+                <ul>
+                    <li>Inicio</li>
+                    <li>Sobre nosotros</li>
+                    <li>Directorio</li>
+                    <li>Mas...</li>
+                    <li>Mas...</li>
+                </ul>
+
+            </section>
+
+            <section>
+                <p>&copy; 11/2023 | Autor: Luciano Gramajo</p>
+            </section>
+        </footer>
+    )
 }
 
 export default Footer;
