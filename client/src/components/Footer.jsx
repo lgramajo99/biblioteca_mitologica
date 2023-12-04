587484a54a9626d03093937ec7eb05f325f00555
lgramajo99 <lucianongramajo@gmail.com>
2023-12-04 17:06:03 -0300

    Footer listo, faltan agregar detalles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,34 +1,37 @@
 function Footer() {
     return (
-        <footer>
-            <section>
-                <p>Seccion de redes sociales.</p>
+        <footer className="bg-gray-800 text-white py-8 my-1">
+            <div className="container mx-auto flex flex-wrap justify-around items-center">
+                {/* Sección de redes sociales */}
+                <section>
+                    <p className="mb-2">Sección de redes sociales.</p>
+                    <div>
+                        <a href="#" target="_blank" rel="noopener noreferrer">💧</a>
+                        <a href="#" target="_blank" rel="noopener noreferrer">🔥</a>
+                        <a href="#" target="_blank" rel="noopener noreferrer">❄</a>
+                        <a href="#" target="_blank" rel="noopener noreferrer">⚡</a>
+                    </div>
+                </section>
 
-                <a href="*" target="_blank" rel="noopener noreferrer">💧</a>
-                <a href="*" target="_blank" rel="noopener noreferrer">🔥</a>
-                <a href="*" target="_blank" rel="noopener noreferrer">❄</a>
-                <a href="*" target="_blank" rel="noopener noreferrer">⚡</a>
+                {/* Sección de sobre el blog */}
+                <section>
+                    <p className="mb-2">Sección de sobre el blog.</p>
+                    <ul className='flex space-x-4'>
+                        <li>Inicio</li>
+                        <li>Sobre nosotros</li>
+                        <li>Directorio</li>
+                        <li>Mas...</li>
+                        <li>Mas...</li>
+                    </ul>
+                </section>
 
-            </section>
-
-            <section>
-                <p>Seccion de sobre el blog.</p>
-
-                <ul>
-                    <li>Inicio</li>
-                    <li>Sobre nosotros</li>
-                    <li>Directorio</li>
-                    <li>Mas...</li>
-                    <li>Mas...</li>
-                </ul>
-
-            </section>
-
-            <section>
-                <p>&copy; 11/2023 | Autor: Luciano Gramajo</p>
-            </section>
+                {/* Sección de derechos de autor */}
+                <section className="w-full">
+                    <p>&copy; 11/2023 | Autor: Luciano Gramajo</p>
+                </section>
+            </div>
         </footer>
-    )
+    );
 }
 
 export default Footer;
