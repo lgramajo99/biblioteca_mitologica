dafe11d5967795d00f22d9f2e9158060c3969121
lgramajo99 <lucianongramajo@gmail.com>
2023-12-06 16:23:32 -0300

    mejorando el boton del filtro

--- client/src/components/PandoraFilter.jsx
+++ client/src/components/PandoraFilter.jsx
@@ -8,8 +8,17 @@ function PandoraFilter() {
     return (
         <div>
 
-            <button type="button" onClick={handleFilterToggle} data-toggle="dropdown" title="None selected" aria-expanded="false">
+            <button type="button"
+                className="flex items-center"
+                onClick={handleFilterToggle}
+                data-toggle="dropdown"
+                title="None selected"
+                aria-expanded="false">
+
                 <span><b>Tipos:</b>Todos</span>
+                {isOpen ? <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-down" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 9l6 6l6 -6" /></svg> //chevron-down 
+                    : <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-up" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 15l6 -6l6 6" /></svg> //chevron-up
+                }
             </button>
 
             {isOpen && <ul>
