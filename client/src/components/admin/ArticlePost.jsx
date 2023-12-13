3307c990d81f6eac8a49e075a9b5557861bd83d8
lgramajo99 <lucianongramajo@gmail.com>
2023-12-13 19:56:28 -0300

    Se crea el componente ArticlePost con la libreria react-quill

--- client/src/components/admin/ArticlePost.jsx
+++ client/src/components/admin/ArticlePost.jsx
@@ -3,17 +3,50 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Importa los estilos predeterminados
 
 function ArticlePost() {
-    const [content, setContent] = useState('');
+    const initialValues = {
+        text: '',
+        mitology: '',
+    };
+
+    const [formData, setFormData] = useState({ ...initialValues });
 
     const handleChange = (value) => {
-        setContent(value);
+        setFormData({
+            ...formData,
+            text: value,
+        });
+    };
+
+    const handleSelectChange = (event) => {
+        setFormData({
+            ...formData,
+            mitology: event.target.value,
+        });
+    };
+
+    const handleSubmit = () => {
+        console.log('Contenido enviado:', formData);
+        setFormData({ ...initialValues });
     };
 
     return (
         <div>
             <h1>Editor de Mitologías</h1>
+
             <div className="h-screen">
-                <ReactQuill value={content} onChange={handleChange} />
+
+                <select name="mitology" value={formData.mitology} onChange={handleSelectChange}>
+                    <option value="">Selecciona...</option>
+                    <option value="option1">Opcion 1</option>
+                    <option value="option2">Opcion 2</option>
+                    <option value="option3">Opcion 3</option>
+                </select>
+
+                <ReactQuill value={formData.text} onChange={handleChange} />
+
+                <button onClick={handleSubmit} className="font-bold p-2 rounded">
+                    Enviar Contenido
+                </button>
             </div>
         </div>
     );
