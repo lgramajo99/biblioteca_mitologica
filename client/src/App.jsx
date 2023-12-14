9d32dd1173a70bd8c49cddb2a00851c89f2cd6c6
lgramajo99 <lucianongramajo@gmail.com>
2023-12-13 21:09:01 -0300

    se agrega la libreria react-router, y arreglamos el componente PandoraFilter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import ArticlePost from './components/admin/ArticlePost'
@@ -9,11 +10,13 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* <Home /> */}
-      {/* <Directory /> */}
-      <ArticlePost />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/directory" element={<Directory />} />
+        <Route path="/article" element={<ArticlePost />} />
+      </Routes>
       <Footer />
-    </div>
+    </div >
   )
 }
 
--- client/src/components/PandoraFilter.jsx
+++ client/src/components/PandoraFilter.jsx
@@ -1,53 +1,94 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
+function Checkbox({ value, id, name, label }) {
+    return (
+        <li>
+            <a tabIndex="0">
+                <input type="checkbox" value={value} id={id} name={name} />
+                <label className="checkbox" htmlFor={id}>
+                    {label}
+                </label>
+            </a>
+        </li>
+    );
+}
 
 function PandoraFilter() {
-    const [isOpen, setOpen] = useState(false)
-    const handleFilterToggle = () => { setOpen(!isOpen) };
+    const [isOpen, setOpen] = useState(false);
+
+    const handleFilterToggle = () => {
+        setOpen(!isOpen);
+    };
+
+    const checkboxes = [
+        { value: "nordico", id: "NordicoCheckbox", label: "Nordico" },
+        { value: "egipcio", id: "EgipcioCheckbox", label: "Egipcio" },
+        { value: "maya", id: "MayaCheckbox", label: "Maya" },
+        { value: "griega", id: "GriegaCheckbox", label: "Griega" },
+        { value: "japonesa", id: "JaponesaCheckbox", label: "Japonesa" },
+        { value: "inca", id: "IncaCheckbox", label: "Inca" },
+        { value: "celta", id: "CeltaCheckbox", label: "Celta" },
+        { value: "azteca", id: "AztecaCheckbox", label: "Azteca" },
+    ];
 
     return (
         <div>
-
-            <button type="button"
+            <button
+                type="button"
                 className="flex items-center"
                 onClick={handleFilterToggle}
                 data-toggle="dropdown"
                 title="None selected"
-                aria-expanded="false">
-
-                <span><b>Tipos:</b>Todos</span>
-                {isOpen ? <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-down" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 9l6 6l6 -6" /></svg> //chevron-down 
-                    : <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-up" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 15l6 -6l6 6" /></svg> //chevron-up
-                }
+                aria-expanded={isOpen}
+                id="tiposButton"
+            >
+                <span>
+                    <b>Tipos:</b>Todos
+                </span>
+                {isOpen ? (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-chevron-down"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M6 9l6 6l6 -6" />
+                    </svg>
+                ) : (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-chevron-up"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M6 15l6 -6l6 6" />
+                    </svg>
+                )}
             </button>
 
-            {isOpen && <ul>
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="nordico" />Nordico</label></a></li>
-
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="egipcio" />Egipcio</label></a></li>
-
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="maya" />Maya</label></a></li>
-
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="griega" />Griega</label></a></li>
-
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="japonesa" />Japonesa</label></a></li>
-
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="inca" />Inca</label></a></li>
-
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="celta" />Celta</label></a></li>
-
-                <li><a tabindex="0"><label className="checkbox">
-                    <input type="checkbox" value="azteca" />Azteca</label></a></li>
-            </ul>
-            }        </div>
-    )
+            {isOpen && (
+                <ul>
+                    {checkboxes.map((checkbox) => (
+                        <Checkbox key={checkbox.id} {...checkbox} />
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
 
 export default PandoraFilter;
