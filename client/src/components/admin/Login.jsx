3c4819f781fa289023071de76d246a694fe40364
lgramajo99 <lucianongramajo@gmail.com>
2024-01-06 18:47:53 -0300

    Componente Login estructurado listo.

---
+++ client/src/components/admin/Login.jsx
@@ -0,0 +1,55 @@
+function Login() {
+    const verify = {
+        email: 'lucianogramajo@gmail.com',
+        password: 'asd123'
+    }
+
+    return (
+        <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
+
+            <h1>Inicio de sesión Biblioteca Mitologica.</h1>
+
+            <svg width="80px" height="80px" viewBox="0 0 1024 1024" className="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M512 960c-92.8 0-160-200-160-448S419.2 64 512 64s160 200 160 448-67.2 448-160 448z m0-32c65.6 0 128-185.6 128-416S577.6 96 512 96s-128 185.6-128 416 62.4 416 128 416z" fill="#050D42" /><path d="M124.8 736c-48-80 92.8-238.4 307.2-363.2S852.8 208 899.2 288 806.4 526.4 592 651.2 171.2 816 124.8 736z m27.2-16c33.6 57.6 225.6 17.6 424-97.6S905.6 361.6 872 304 646.4 286.4 448 401.6 118.4 662.4 152 720z" fill="#050D42" /><path d="M899.2 736c-46.4 80-254.4 38.4-467.2-84.8S76.8 368 124.8 288s254.4-38.4 467.2 84.8S947.2 656 899.2 736z m-27.2-16c33.6-57.6-97.6-203.2-296-318.4S184 246.4 152 304 249.6 507.2 448 622.4s392 155.2 424 97.6z" fill="#050D42" /><path d="M512 592c-44.8 0-80-35.2-80-80s35.2-80 80-80 80 35.2 80 80-35.2 80-80 80zM272 312c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48zM416 880c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48z m448-432c-27.2 0-48-20.8-48-48s20.8-48 48-48 48 20.8 48 48-20.8 48-48 48z" fill="#2F4BFF" /></svg>
+
+            <form className="d-flex flex-col " action="">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold"
+                        htmlFor="password">Correo electrónico</label>
+
+                    <input className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                        placeholder="Example: ejemplo@email.com"
+                        type="email"
+                        name="email"
+                        id="email" />
+                </div>
+
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold"
+                        htmlFor="password">Contraseña</label>
+                    <input className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                        placeholder="**********"
+                        type="password"
+                        name="password" />
+                </div>
+                <div className="mb-4 flex items-center">
+                    <input
+                        className="mr-2 leading-tight"
+                        type="checkbox"
+                        id="remember"
+                        name="remember"
+                    />
+                    <label className="text-sm text-gray-700" htmlFor="remember">
+                        Recordar
+                    </label>
+                </div>
+                <div className="flex justify-end">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                        Iniciar sesión
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import AsideBar from "../components/AsideBar";
 import Manufactures from "../components/Manufactures";
+import Login from "../components/admin/Login";
 
 function Home() {
     return (
         <section className="text-center sm:">
             <h1 className="">Biblioteca mitologica</h1>
             <secction className="flex flex-wrap">
+
+                <Login />
 
                 <article className="sm:w-full md:w-9/12 lg:w-10/12">
                     <Manufactures seccion={'Mas recientes'} />
