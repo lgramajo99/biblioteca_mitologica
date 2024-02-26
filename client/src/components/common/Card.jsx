12a57e4e96fb7583db586e71f343ee12a3dabbf9
lgramajo99 <lucianongramajo@gmail.com>
2024-02-26 16:08:20 -0300

    Card mejorada, estructura y logica

--- client/src/components/common/Card.jsx
+++ client/src/components/common/Card.jsx
@@ -1,31 +1,30 @@
-
 function Card({ data }) {
-
+    if (!data) return null;
     const { id, titulo, frase, imagenes } = data;
 
     return (
-        <div className="container mx-auto p-4" key={id}>
-            <div className="bg-white pb-4 rounded-lg shadow-xl">
-                <img src={imagenes?.imagen}
-                    alt={imagenes?.alt}
-                    className="rounded w-full h-64 object-cover" />
+        <article className="bg-white rounded-lg shadow-xl overflow-hidden flex flex-col h-full" key={id}>
+            <header>
+                {imagenes && (<img
+                    src={imagenes?.imagen || ''}
+                    alt={imagenes?.alt || ''}
+                    className="w-full h-64 object-cover overflow-hidden"
+                />)}
 
                 <h3 className="mt-2 line-clamp-2">{titulo}</h3>
-
-                <p className="text-gray-600 mt-2 line-clamp-3">
-                    {frase}
-                </p>
-
-                <div className="mt-4 mx-4 text-right">
-                    <a className="text-blue-500 font-bold underline hover:text-blue-700"
-                        href="/articulo-prueba"
-                        target="_blank">
-                        Leer más...
-                    </a>
-                </div>
-            </div>
-        </div>
+                <p className="text-gray-600 mt-2 line-clamp-3">{frase}</p>
+            </header>
+            <footer className="text-right mt-auto py-1 px-2">
+                <a className="text-blue-500 font-bold underline hover:text-blue-700 mb-auto"
+                    href="/articulo-prueba"
+                    rel="noopener noreferrer"
+                    target="_blank">
+                    Leer más...
+                </a>
+            </footer>
+        </article>
     );
 }
+
 
 export default Card;
